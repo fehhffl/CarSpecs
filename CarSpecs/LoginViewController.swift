7d75bd0011fa433c07d6056095f52497246adfa8
Felipe Lima <fe.forioni@gmail.com>
2023-08-07 20:02:55 -0300

    Create a welcome and login screen.

---
+++ CarSpecs/LoginViewController.swift
@@ -0,0 +1,60 @@
+//
+//  LoginViewController.swift
+//  CarSpecs
+//
+//  Created by Felipe Lima on 03/08/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var userTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordErrorLabel: UILabel!
+    @IBOutlet weak var logInButton: UIButton!
+    let user = "Felipe Lima"
+    let password = "Banana123"
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        title = "Sign In"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        passwordErrorLabel.isHidden = true
+        
+    }
+    @IBAction func onLogInButtonTapped(_ sender: Any) {
+        if userTextField.text == user,
+           passwordTextField.text == password {
+            passwordErrorLabel.isHidden = false
+            passwordErrorLabel.text = "Success"
+            passwordErrorLabel.textColor = .green
+        } else {
+            passwordErrorLabel.isHidden = false
+            passwordErrorLabel.text = "Wrong password or email!"
+            passwordErrorLabel.textColor = .red
+            
+        }
+           
+    }
+    
+        func setupButtons() {
+            logInButton.layer.cornerRadius = logInButton.frame.size.height/2
+            passwordTextField.layer.cornerRadius = passwordTextField.frame.size.height/2
+            passwordTextField.layer.borderWidth = 1
+            passwordTextField.layer.borderColor = UIColor.lightGray.cgColor
+            passwordTextField.clipsToBounds = true
+            userTextField.layer.cornerRadius = userTextField.frame.size.height/2
+            userTextField.layer.borderWidth = 1
+            userTextField.layer.borderColor = UIColor.lightGray.cgColor
+            userTextField.clipsToBounds = true
+        }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupButtons()
+        
+    }
+
+
+   
+
+}
---
+++ CarSpecs/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  CarSpecs
+//
+//  Created by Felipe Lima on 18/07/23.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    
+    func setUpViews() {
+        loginButton.layer.cornerRadius = loginButton.frame.size.height/2
+        subtitleLabel.text = "first_screen_message".localize()
+        titleLabel.text = "first_screen_message_welcome".localize()
+        loginButton.setTitle("first_screen_message_log_in".localize(), for: .normal)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpViews()
+       
+    }
+    
+    @IBAction func onLoginButtonTapped(_ sender: Any) {
+        navigationController?.pushViewController(LoginViewController(), animated: true)
+    }
+
+}
+
+
+extension String {
+    func localize() -> String {
+        return NSLocalizedString(self, comment: "")
+    }
+}
+
+
