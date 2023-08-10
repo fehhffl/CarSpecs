b36feb15b958abefc0a7640e52a1ead4bfbc124c
Felipe Lima <fe.forioni@gmail.com>
2023-08-10 19:17:10 -0300

    Localize home strings, fix previous commit issues.

--- CarSpecs/LoginViewController.swift
+++ CarSpecs/LoginViewController.swift
@@ -8,21 +8,21 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-    @IBOutlet weak var userTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var passwordErrorLabel: UILabel!
-    @IBOutlet weak var logInButton: UIButton!
-    let user = "fe.forioni@gmail.com"
-    let password = "Banana123"
+    @IBOutlet private weak var userTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var passwordErrorLabel: UILabel!
+    @IBOutlet private weak var logInButton: UIButton!
+    private let user = "fe.forioni@gmail.com"
+    private let password = "Banana123"
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         title = "Sign In"
         navigationController?.navigationBar.prefersLargeTitles = true
         passwordErrorLabel.isHidden = true
-
     }
 
-    @IBAction func onLogInButtonTapped(_ sender: Any) {
+    @IBAction private func onLogInButtonTapped(_ sender: Any) {
         if userTextField.text == user,
            passwordTextField.text == password {
             passwordErrorLabel.isHidden = false
@@ -33,20 +33,21 @@ class LoginViewController: UIViewController {
             passwordErrorLabel.isHidden = false
             passwordErrorLabel.text = "Wrong password or email!"
             passwordErrorLabel.textColor = .red
-
         }
     }
-        func setupButtons() {
-            logInButton.layer.cornerRadius = logInButton.frame.size.height/2
-            passwordTextField.layer.cornerRadius = passwordTextField.frame.size.height/2
-            passwordTextField.layer.borderWidth = 1
-            passwordTextField.layer.borderColor = UIColor.lightGray.cgColor
-            passwordTextField.clipsToBounds = true
-            userTextField.layer.cornerRadius = userTextField.frame.size.height/2
-            userTextField.layer.borderWidth = 1
-            userTextField.layer.borderColor = UIColor.lightGray.cgColor
-            userTextField.clipsToBounds = true
-        }
+
+    private func setupButtons() {
+        logInButton.layer.cornerRadius = logInButton.frame.size.height / 2
+        passwordTextField.layer.cornerRadius = passwordTextField.frame.size.height / 2
+        passwordTextField.layer.borderWidth = 1
+        passwordTextField.layer.borderColor = UIColor.lightGray.cgColor
+        passwordTextField.clipsToBounds = true
+        userTextField.layer.cornerRadius = userTextField.frame.size.height / 2
+        userTextField.layer.borderWidth = 1
+        userTextField.layer.borderColor = UIColor.lightGray.cgColor
+        userTextField.clipsToBounds = true
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupButtons()
